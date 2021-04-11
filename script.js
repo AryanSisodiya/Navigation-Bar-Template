fdee056bc59678f78cb2579236d66071bcac693d
AryanSisodiya <aryansisodiya091107@gmail.com>
2021-04-11 11:36:22 +0530

    NavBar Upgation in process...

--- script.js
+++ script.js
@@ -3,13 +3,22 @@ let div = document.getElementById('div');
 // Close Burger
 let closeBtn = document.getElementById('close-burger-li')
 closeBtn.addEventListener('click', () => {
-    div.style.animation = 'menu-close 2s ease-in-out';
-
+    div.style.animation = 'menu-close 1s ease-in-out';
+    setTimeout(() => {
+        div.style.display = 'none'
+    }, 1000);
+    
 });
 
 // Open Burger
 let openBtn = document.getElementById('open-burger');
 openBtn.addEventListener('click', () => {
-    div.style.animation = 'menu-open 2s ease-in-out';
+    div.style.display = 'flex'
+    div.style.animation = 'menu-open 1s ease-in-out';
+    div.style.right = '0px';
+    // div.style.transition = '1s ease-in-out'
+    // setTimeout(() => {
+    //     div.style.display = 'flex'
+    // }, 1000);
 })
 
